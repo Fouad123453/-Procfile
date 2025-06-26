85d39b6895ce36f8f4ab3a954a31603bbd055040
Fouad123453 <fouadzara921@gmail.com>
2025-06-26 12:07:47 +0100

    wilayas.py

---
+++ wilayas.py
@@ -0,0 +1,18 @@
+# wilayas.py
+wilayas = {
+    "أدرار": "Adrar", "الشلف": "Chlef", "الأغواط": "Laghouat", "أم البواقي": "Oum El Bouaghi",
+    "باتنة": "Batna", "بجاية": "Bejaia", "بسكرة": "Biskra", "بشار": "Bechar", "البليدة": "Blida",
+    "البويرة": "Bouira", "تمنراست": "Tamanrasset", "تبسة": "Tebessa", "تلمسان": "Tlemcen",
+    "تيارت": "Tiaret", "تيزي وزو": "Tizi Ouzou", "الجزائر": "Algiers", "الجلفة": "Djelfa",
+    "جيجل": "Jijel", "سطيف": "Setif", "سعيدة": "Saida", "سكيكدة": "Skikda", "سيدي بلعباس": "Sidi Bel Abbes",
+    "عنابة": "Annaba", "قالمة": "Guelma", "قسنطينة": "Constantine", "المدية": "Medea", 
+    "مستغانم": "Mostaganem", "المسيلة": "M'sila", "معسكر": "Mascara", "ورقلة": "Ouargla",
+    "وهران": "Oran", "البيض": "El Bayadh", "إليزي": "Illizi", "برج بوعريريج": "Bordj Bou Arreridj",
+    "بومرداس": "Boumerdes", "الطارف": "El Tarf", "تندوف": "Tindouf", "تيسمسيلت": "Tissemsilt",
+    "الوادي": "El Oued", "خنشلة": "Khenchela", "سوق أهراس": "Souk Ahras", "تيبازة": "Tipaza",
+    "ميلة": "Mila", "عين الدفلى": "Ain Defla", "النعامة": "Naama", "عين تموشنت": "Ain Temouchent",
+    "غرداية": "Ghardaia", "غليزان": "Relizane", "تميمون": "Timimoun", "برج باجي مختار": "Bordj Badji Mokhtar",
+    "أولاد جلال": "Ouled Djellal", "بني عباس": "Beni Abbes", "عين صالح": "Ain Salah",
+    "عين قزام": "Ain Guezzam", "تقرت": "Touggourt", "جانت": "Djanet", "المغير": "El Meghaier",
+    "المنيعة": "El Meniaa"
+}
